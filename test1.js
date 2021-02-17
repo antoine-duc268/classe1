1041ae091374bf8d4110f9299e97bf69c4613368
Antoine Duchaine <antoine.duc.268@gmail.com>
2021-02-17 11:38:41 -0500

    Ajout du reste du code

--- test1.js
+++ test1.js
@@ -15,8 +15,6 @@ class EmployeUniversite {
     }
 }
 
-// test
-
 class EmployeUniversiteProfesseur extends EmployeUniversite {
     constructor(nom, prenom, numeroEmploye, anneesExperience, salaire, coursEnseigne) {
         super(nom, prenom, numeroEmploye, anneesExperience);
@@ -28,3 +26,20 @@ class EmployeUniversiteProfesseur extends EmployeUniversite {
         this.coursEnseigne[this.coursEnseigne.length] = nouveauCours;
     }
 }
+
+employe = new EmployeUniversite('Pierre', 'Martel', 'E0004', 5);
+employe.augmenterAnciennete();
+console.log(employe.nomComplet());
+console.log(employe.anneesExperience);
+
+prof = new EmployeUniversiteProfesseur('Carmel', 'Simon', 'P0007', 4, 65000, []);
+prof.augmenterAnciennete();
+console.log(prof.nomComplet());
+console.log(prof.anneesExperience);
+console.log(prof.salaire);
+console.log(prof.coursEnseigne);
+
+prof.ajouterUnCours('2RP');
+console.log(prof.coursEnseigne);
+prof.ajouterUnCours('CG2');
+console.log(prof.coursEnseigne);
